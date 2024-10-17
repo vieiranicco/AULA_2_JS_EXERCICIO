38185cb1787122b95d91ff1a3a3a189a4805b37d
vieiranicco <nicollas.contato@hotmail.com>
2024-10-16 23:32:55 -0300

    exercicio_aula_02

---
+++ exercicio.js
@@ -0,0 +1,9 @@
+var diaAniv = prompt("Insira o dia do seu Aniversário");
+var mesAniv = prompt("Insira o mês do seu Anivesário");
+var anoNasc = prompt("Insira o ano em que você Nasceu")
+
+
+var dataCompleta = diaAniv + " de " + mesAniv + " de " + anoNasc;
+
+alert(dataCompleta);
+console.log(diaAniv, "de", mesAniv, "de", anoNasc)
